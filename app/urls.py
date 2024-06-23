0217ff68c18f477d2f8709966b93c97ae5c92dc1
hanami_jr <hanami_jr@mail.ru>
2024-06-23 22:56:06 +0300

    Добавлено приложение goods

--- app/urls.py
+++ app/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls', namespace='main'))
+    path('', include('main.urls', namespace='main')),
+    path('catalog/', include('goods.urls', namespace='catalog'))
 ]
